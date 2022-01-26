ab6d28f83e1c4654679ad15119c8087c642325ee
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-26 13:10:42 +0200

    Initial Commit

---
+++ P11-02-TextEditor/P11_02_TextEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  P11_02_TextEditorApp.swift
+//  P11-02-TextEditor
+//
+//  Created by Vahtee Boo on 26.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct P11_02_TextEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
